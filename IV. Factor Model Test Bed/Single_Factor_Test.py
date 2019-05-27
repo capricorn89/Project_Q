01489a369b940eba23d1f2fd401785d7a029d50f
capricorn89 <capricorn8911@gmail.com>
2019-05-27 17:48:51 +0900

    code modified

--- IV. Factor Model Test Bed/Single_Factor_Test.py
+++ IV. Factor Model Test Bed/Single_Factor_Test.py
@@ -65,33 +65,36 @@ def get_recentBday(date, dateFormat = 'sql'):
     
     return data
 
-
-
-
-start_year = 2007
-start_month = 1
-start_day = calendar.monthrange(start_year, start_month)[1]
-
-end_year = 2019
-end_month = 3
-end_day = calendar.monthrange(end_year, end_month)[1]
-
-start_invest = pd.datetime(start_year, start_month, start_day)
-end_invest = pd.datetime(end_year, end_month, end_day)
-rebal_sche = pd.date_range(start_invest, end_invest, freq = 'M')
-
-
-os.chdir('C:/Woojin/###. Git/Project_Q/IV. Factor Model Test Bed')
-
-factor = data_cleansing(pd.read_excel('testData.xlsx', sheet_name = 'factor'))
-marketInfo = data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'market'))
-
-
-def getUniverse(marketInfoData, rebalDate, universeName = 'KOSPI'):
-    '''market에서 유니버스 이름(ex, KOSPI)로 필터링'''    
-    m = marketInfoData.loc[:rebalDate, :].iloc[-1,:]
-    m = m[m == universeName]
-    return m.index.values
+def getUniverse(marketInfoData, mktcapData, riskInfo_1, riskInfo_2, rebalDate_, 
+                universeName = 'KOSPI', mktcapLimit = 2000):
+    '''market에서 KOSPI인 종목 중에서
+    거래정지, 관리종목인 종목 거르고,
+    시가총액 2000억 미만 거르고, 
+    20일 평균거래대금 10억 이상으로 필터링'''
+    
+    # 상장시장
+    m = marketInfoData.loc[rebalDate_, :]
+    inMarket = m[m == universeName].index.values
+    
+    # 거래정지
+    notRisk_1 = riskInfo_1.loc[rebalDate_, :]
+    notRisk_1 = notRisk_1[notRisk_1 == 0].index.values
+    
+    #관리종목
+    notRisk_2 = riskInfo_2.loc[rebalDate_, :]
+    notRisk_2 = notRisk_2[notRisk_2 == 0].index.values
+    
+#    # 거래대금 기준
+#    v = volInfo.loc[rebalDate_, :]
+#    above10 = v[v >= tradeVolLimit].index.values
+#    
+    # 시총기준
+    cap = mktcapData.loc[rebalDate_, :]
+    cap = cap[cap >= mktcapLimit].index.values
+           
+    res = set(set(set(inMarket).intersection(notRisk_1)).intersection(notRisk_2)).intersection(cap)
+    
+    return list(res)
 
 def getFactorData(factorData, rebalDate, dataPeriod='Q'):
 
@@ -140,32 +143,183 @@ def getFactorData(factorData, rebalDate, dataPeriod='Q'):
 
     t_day = calendar.monthrange(t_year, t_month)[1]  
     date_available = pd.datetime(t_year, t_month, t_day)
-    data = factorData.loc[:date_available,:].iloc[-1,:]
+    data = factorData.loc[:date_available,:].iloc[-1,:].dropna()
 
     return data
         
+def get_stock_price(stockCodes, date_start, date_end):
+    '''
+    input: 
+    - stockcodes : list
+    - date_start : datetime
+    - date_end : datetiem
+    
+    output : DataFrame
+    - Index : Stock code
+    - value : Stock Price
+    '''
+    date_start = ''.join([x for x in str(date_start)[:10] if x != '-'])
+    date_end = ''.join([x for x in str(date_end)[:10] if x != '-'])
+    db = pymysql.connect(host='192.168.1.190', port=3306, user='root', passwd='gudwls2@', db='quant_db',charset='utf8',autocommit=True)
+    cursor = db.cursor()
+    joined = "\',\'".join(stockCodes)
+    sql = "SELECT GICODE, TRD_DT, ADJ_PRC FROM dg_fns_jd WHERE TRD_DT BETWEEN " + date_start
+    sql += ' AND ' + date_end
+    sql += (" AND GICODE IN (\'" + joined + "\')")
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    data = pd.DataFrame(list(data))
+    data = data.pivot(index = 1, columns = 0, values = 2)
+    data.index = pd.to_datetime(data.index.values)
+    db.close()   
+    return data
 
 
-def psr(factor, universe):
+
+
+def get_priceRatio(priceData, factorData, factorName):      
+  
+    code_f = factorData.index.values
+    code_p = priceData.columns.values
+    code = list(set(code_f).intersection(code_p))
     
+    ratioData = pd.DataFrame(index = code)
+    ratioData[factorName] = factorData[code].values
+    ratioData['price'] = priceData[code].values[0]
     
-    factor = factor[universe]
+    ratioData = ratioData[ratioData[factorName] != 0]
         
+    try:    
+        ratioData['ratio'] = ratioData['price'].divide(ratioData[factorName])
+        #print(ratioData)
+    except ZeroDivisionError:
+        print('division Error')
     
+    return ratioData['ratio']
+
+
+
+def get_longshort(ratioData, num_group = 10, ascending = True, sector_on = False, allGroup = False):
     
+    ratioData = ratioData[ratioData>0]
+    ratioData = pd.DataFrame(ratioData)
     
+    '''ratio를 z-score로 전환후 랭킹 출력'''
     
+    if sector_on == True:
+        pass
+    
+    else:
+        if allGroup == False:
+            ratioData['group'] = pd.qcut(ratioData.values.squeeze(), q = num_group, labels = False)
+            
+            if ascending == True: # ratio가 작은 그룹을 Long
+                
+                group_long = ratioData[ratioData['group'] == 0]
+                group_short = ratioData[ratioData['group'] == num_group-1]
+            
+            else:
+                group_long = ratioData[ratioData['group'] == num_group-1]
+                group_short = ratioData[ratioData['group'] == 0]      
+        
+            return group_long.index.values, group_short.index.values
+        
+        else:   # qcut의 모든 그룹을 다 담음
+            ratioData['group'] = pd.qcut(ratioData.values.squeeze(), q = num_group, labels = False)
+            group_n = {}
+            if ascending == True: 
+                for group in range(num_group):                    
+                    group_n[group] = ratioData[ratioData['group'] == group]
+                
+            else:
+                for group in range(num_group):
+                    group_n[group] = ratioData[ratioData['group'] == num_group-group]
+            
+            return group_n
+                    
+        
+
+
+        
+def to_backtestFormat(long_test, rebalDate):
+
+    longData = pd.DataFrame(index = range(len(long_test)))   
+    longData['date'] = rebalDate    
+    longData['code'] = long_test    
+    longData['weight'] = np.ones(len(long_test)) / len(long_test)
+
+    return longData[['date','code','weight']]    
+            
+            
+    
+os.chdir('C:/Woojin/###. Git/Project_Q/IV. Factor Model Test Bed')
+
+start_year = 2006
+start_month = 12
+start_day = calendar.monthrange(start_year, start_month)[1]
+
+end_year = 2019
+end_month = 3
+end_day = calendar.monthrange(end_year, end_month)[1]
+
+start_invest = pd.datetime(start_year, start_month, start_day)
+end_invest = pd.datetime(end_year, end_month, end_day)
+rebal_sche = pd.date_range(start_invest, end_invest, freq = 'M')
+
+factor = data_cleansing(pd.read_excel('testData.xlsx', sheet_name = 'sales'))
+marketInfo = data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'market'))
+mktcap = data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'mktcap'))
+risk_1 = data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'risk_1'))
+risk_2 = data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'risk_2'))
+
+
+rebalData_long = []
+rebalData_short = []
+
+for i in range(len(rebal_sche)):
+    
+    rebalDate_ = get_recentBday(rebal_sche[i], dateFormat = 'datetime')
+    print(rebalDate_)
+    factor_ = getFactorData(factor, rebalDate_)
+    univ_ = getUniverse(marketInfo, mktcap, risk_1, risk_2, rebalDate_)
+    price_ = get_stock_price(factor_.index.values, rebalDate_, rebalDate_)
+    ratio_ = get_priceRatio(price_, factor_, 'sales')
+    long, short = get_longshort(ratio_, 10)
+    longFinal = to_backtestFormat(long, rebalDate_)
+    shortFinal = to_backtestFormat(short, rebalDate_)
+    
+    rebalData_long.append(longFinal)
+    rebalData_short.append(shortFinal)
+   
+
+rebalData_long = pd.concat(rebalData_long).reset_index()[['date','code','weight']]
+rebalData_short = pd.concat(rebalData_short).reset_index()[['date','code','weight']]
+
+rebalData_long.to_excel('backTest_PSR_long.xlsx')
+rebalData_short.to_excel('backTest_PSR_short.xlsx')
 
 
 
+import backtest_pipeline as bt
+import util
 
+res_long = bt.get_backtest_history(1000, rebalData_long)
+res_short = bt.get_backtest_history(1000, rebalData_short)
 
+results_l = res_long[0]
+results_s = res_short[0]
 
+result = pd.concat([results_l, results_s], axis  = 1)
+result.columns = ['long', 'short']
+result['longShort_return'] = np.subtract(result.pct_change()['long'],result.pct_change()['short'])
 
+bm = util.get_index_price(['I.001','I.101'], results_l.index[0], results_l.index[-1])/100
 
+result = pd.concat([result, bm], axis = 1)
 
+result.to_excel('backtest_result.xlsx')
 
-
+(result['longShort_return']+ 1).cumprod().plot()
 
 
 
