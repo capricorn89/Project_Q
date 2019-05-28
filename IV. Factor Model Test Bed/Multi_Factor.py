ba83ad04776d5a884c4fe7b81f240b29c96358ce
capricorn89 <capricorn8911@gmail.com>
2019-05-28 17:59:43 +0900

    muti factor model started

--- IV. Factor Model Test Bed/Multi_Factor.py
+++ IV. Factor Model Test Bed/Multi_Factor.py
@@ -20,3 +20,22 @@ from scipy import stats
 import backtest_pipeline as bt
 import util
 
+os.chdir('C:/Woojin/###. Git/Project_Q/IV. Factor Model Test Bed')
+
+start_year, start_month = 2006, 12
+start_day = calendar.monthrange(start_year, start_month)[1]
+end_year, end_month = 2019, 3
+end_day = calendar.monthrange(end_year, end_month)[1]
+
+start_invest = pd.datetime(start_year, start_month, start_day)
+end_invest = pd.datetime(end_year, end_month, end_day)
+rebal_sche = pd.date_range(start_invest, end_invest, freq = 'M')
+
+marketInfo = util.data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'market'))
+mktcap = util.data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'mktcap'))
+risk_1 = util.data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'risk_1'))
+risk_2 = util.data_cleansing_ts(pd.read_excel('testData.xlsx', sheet_name = 'risk_2'))
+
+factor_PSR = util.data_cleansing(pd.read_excel('testData.xlsx', sheet_name = 'sales'))
+factor_PBR = util.data_cleansing(pd.read_excel('testData.xlsx', sheet_name = 'book'))
+factor_PER = util.data_cleansing(pd.read_excel('testData.xlsx', sheet_name = 'earnings'))
