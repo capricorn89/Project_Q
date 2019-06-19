ff6d9609c3c5276895764988a6316ff12642bdfb
capricorn89 <capricorn8911@gmail.com>
2019-06-19 11:10:33 +0900

    Update Multifactor_with_Macro.py

--- II. Adaptive Multi Factor Strategy/Multifactor_with_Macro.py
+++ II. Adaptive Multi Factor Strategy/Multifactor_with_Macro.py
@@ -294,8 +294,10 @@ from bokeh.models import LinearAxis, Range1d
 from bokeh.transform import factor_cmap
 from bokeh.palettes import Spectral10
 from bokeh.models import ColumnDataSource
+from bokeh.models import HoverTool
 
 result = pd.read_excel('res_2019618.xlsx')
+
 
 x = result.index
 y1 = (result['longShort_return'].fillna(0)+1).cumprod() * 100
@@ -304,8 +306,7 @@ y4 = (result['long'].pct_change().fillna(0) + 1).cumprod() * 100
 y3 = y1 - y2
 y5 = y4 - y2
 
-## add a line renderer with legend and line thickness
-p1 = figure(plot_width=800, plot_height=300,
+p1 = figure(plot_width=800, plot_height=300, 
            x_axis_type="datetime", x_axis_label='Date', y_axis_label='Relative Return')
 p1.title.text = "Adaptive Multi Factor Allocatioin using ESI Index (excluding Size factor)"
 p1.title.align = "center"
@@ -326,3 +327,22 @@ p2.varea(x=x, y1 = np.zeros(len(y1)), y2 = util.get_drawdown(y1), legend="Drawdo
 p2.legend.location = "bottom_left"
 
 show(column(p1,p2))
+
+#hover = p1.select(dict(type=HoverTool))
+#hover.tooltips= [("Date", "$x"),
+#                ("Portfolio : ", "$y1"),
+#                ("KOSPI200 : ", "$y2"), 
+#                ("Excess Return : ", "$y3"),
+#                ("Portfolio (Long Only) : ", "$y4"),
+#                ("Excess Return (Long Only) : ", "$y5")]
+#hover.mode = 'mouse' 
+##    formatters = {
+##                 "Date" : 'datetime',
+##                 'y1' : 'printf',
+##                 'y2' : 'printf',
+##                 'y3' : 'printf',
+##                 'y4' : 'printf',
+##                 'y5' : 'printf',
+##                 },
+#show(p1)
+
