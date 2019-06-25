205b8c12ff472e758fccc2a068e3e9a96887a321
capricorn89 <capricorn8911@gmail.com>
2019-06-25 17:49:58 +0900

    New file added

--- V. Small Cap strategy/firm_selection.py
+++ V. Small Cap strategy/firm_selection.py
@@ -40,32 +40,48 @@ mkt_info = ut.data_cleansing(pd.read_excel(rdata, 'market_info'))
 mktcap = ut.data_cleansing(pd.read_excel(rdata, 'mktcap'))
 net_income = ut.data_cleansing_as(pd.read_excel(rdata, 'net_income'))
 vol = ut.data_cleansing(pd.read_excel(rdata, 'volume'))
+caution_1 = ut.data_cleansing(pd.read_excel(rdata, '거래정지'))
+caution_2 = ut.data_cleansing(pd.read_excel(rdata, '투자유의'))
+caution_3 = ut.data_cleansing(pd.read_excel(rdata, '관리종목'))
 
-
-def get_universe(rebalDate, df_info, df_mktcap, df_netincome, df_vol):  
-    # 코스피 대상
+def get_universe(rebalDate, df_info, df_mktcap, df_netincome, df_vol, df_caution_1, df_caution_2, df_caution_3):  
+    
+    # 코스피 중소형주 대상
     lookback_info = df_info.loc[:rebalDate, :].tail(1).transpose()
-    lookback_info = lookback_info[lookback_info == 'KOSPI'].dropna().index.values
+    lookback_info = lookback_info[(lookback_info == 2) |(lookback_info == 3) ].dropna().index.values
 #    print(len(lookback_info))    
     # 3년 연속 적자기업 제외
     lookback_NI = df_netincome.loc[:rebalDate, :].tail(3)
     lookback_NI = (lookback_NI < 0).astype(int).sum()
     lookback_NI = lookback_NI[lookback_NI < 3].dropna().index.values
 #    print(len(lookback_NI))    
-    # 시총 800억 미만 & 200위 이내 기업 제외
+    
+    # 시총 800억 미만 기업 제외
     lookback_mktcap = df_mktcap.loc[:rebalDate, :].tail(1).transpose()
-    lookback_mktcap = lookback_mktcap[lookback_mktcap.rank() > 200].dropna() # 시총 200위 이상만 걸러내기
     lookback_mktcap = lookback_mktcap[lookback_mktcap > 1000].dropna().index.values # 시총 800억 미만 걸러내기
-#    print(len(lookback_mktcap))    
+#    print(len(lookback_mktcap))   
+    
     # 반기 거래대금 3억 미만 기업 제외
     lookback_vol = df_vol.loc[:rebalDate, :].tail(1).transpose()
     lookback_vol = lookback_vol[lookback_vol >= 3].dropna().index.values
+    
+    # 관리종목, 거래정지, 투자유의 종목 제외
+    c1 = df_caution_1.loc[:rebalDate, :].tail(1).transpose()
+    c1 = c1[c1 == 0].dropna().index.values
+    c2 = df_caution_2.loc[:rebalDate, :].tail(1).transpose()
+    c2 = c2[c2 == 0].dropna().index.values
+    c3 = df_caution_3.loc[:rebalDate, :].tail(1).transpose()
+    c3 = c3[c3 == 0].dropna().index.values
+    lookback_caution = list(set(c1).intersection(c2).intersection(c3))
+    
 #    print(len(lookback_vol))    
-    univ = set(lookback_info).intersection(lookback_NI).intersection(lookback_mktcap).intersection(lookback_vol)
+    univ = set(lookback_info).intersection(lookback_NI).intersection(lookback_mktcap).intersection(lookback_vol).intersection(lookback_caution)
     univ = list(univ)       
     return univ
 
-len(get_universe(pd.datetime(2019,5,31), mkt_info, mktcap, net_income, vol))
-        
-        
+len(get_universe(pd.datetime(2019,5,31), mkt_info, mktcap, net_income, vol, caution_1, caution_2, caution_3))
+
+
+
+
         
