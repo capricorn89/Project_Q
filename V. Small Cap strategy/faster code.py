0caa4974faff593d6be2eaa8fdaf7cd80de378d0
capricorn89 <capricorn8911@gmail.com>
2019-07-01 09:59:07 +0900

    Plot modified

---
+++ V. Small Cap strategy/faster code.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 14:42:43 2019
+
+@author: user
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import datetime
+import pickle
+
+path = 'C:/Woojin/###. Git/Project_Q/V. Small Cap strategy/Data'
+os.chdir(path)
+
+
+
+total = pd.read_excel(filename, None)
+
+sheets = list(total.keys())
+
+data = []
+for name in sheets:
+    temp = [name]
+    this = total[name]
+    temp.append(this.iloc[9,4])
+    
+    this.columns = this.iloc[7]
+    this.index = this.iloc[:,1]
+    this = this.iloc[10:,5:]
+    temp.append(this)
+
+    data.append(temp)
+#%%
+filename = 'data.xlsx'
+with open(filename[:filename.find('.')]+'.pkl', 'wb') as f:
+    pickle.dump(data, f)
+  
+
+#%%
+data = pd.read_pickle('data.pkl')
+
+port = data[0][2] == 3  # 소형주만
+port = port & (data[1][2] >= 1000)  # 시총
+
+port = port & (data[3][2] >= 3)  # 거래대금
+port = port & (data[4][2] == 0)  # 거래정지 X
+port = port & (data[5][2] == 0)  # 관리종목 X
+port = port & (data[6][2] == 0)  # 투자유의 X
+
+port = port & (data[7][2] < 20)  # per 20미만
+
+#%%
+
+for i in range(len(port)):
+    
+    dt = port.index[i]
+    
+    port_i = port.loc[dt,:]
+    port_i = port_i[port_i == True].dropna()
+    print(port_i.index)
+    
--- X. Visualization/TimeSeries_1.py
+++ X. Visualization/TimeSeries_1.py
@@ -10,20 +10,23 @@ from bokeh.layouts import column
 from bokeh.models import LinearAxis, Range1d
 from bokeh.models import HoverTool
 from bokeh.plotting import ColumnDataSource, figure
+from bokeh.models.widgets import Tabs, Panel
 import numpy as np
 import pandas as pd
 import os
 import numpy as np
+from bokeh.transform import factor_cmap
+from bokeh.palettes import Spectral10
 
 #import holoviews as hv
 #from holoviews import opts
 
-#os.chdir('C:/Woojin/###. Git/Project_Q/X. Visualization')
-os.chdir('/Users/Woojin/Documents/GitHub/Project_Q/X. Visualization')         
+os.chdir('C:/Woojin/###. Git/Project_Q/X. Visualization')
+#os.chdir('/Users/Woojin/Documents/GitHub/Project_Q/X. Visualization')         
 ts = pd.read_excel('test_timeseries.xlsx', sheet_name = 'Sheet1')
 
 # output to static HTML file
-output_file("lines.html")
+#output_file("lines.html")
 
 # create a new plot with a title and axis labels
 
@@ -54,7 +57,7 @@ y1 = ( ts['long'].pct_change().fillna(0) + 1 ).cumprod() * 100
 y2 = ( ts['I.101'].pct_change().fillna(0) + 1 ).cumprod() * 100
 
 ## add a line renderer with legend and line thickness
-p1 = figure(plot_width=800, plot_height=300, title="simple line example",
+p1 = figure(plot_width=1600, plot_height=500, title="simple line example",
            x_axis_type="datetime", x_axis_label='Date', y_axis_label='Price')
 
 p1.line(x, y1, legend="Portfolio", color = 'red', line_width=2)
@@ -62,56 +65,18 @@ p1.line(x, y2, legend="KOSPI200", color = 'grey', line_width=2)
 p1.legend.location = "top_left"
 p1.legend.click_policy="hide"
 
-p2 = figure(plot_width=800, plot_height=100, x_range = p1.x_range, x_axis_type="datetime")
+p2 = figure(plot_width=1600, plot_height=300, x_range = p1.x_range, x_axis_type="datetime")
 p2.varea(x=x , y1 = np.zeros(len(y1)), y2 = drawdown(y1))
 p1.add_tools(HoverTool(tooltips=[('Date','@x{%F}'),('Price','@y')], 
                                  formatters={'x' : 'datetime'}, mode = 'vline',
                                  show_arrow=False, point_policy='follow_mouse'))
-show(column(p1,p2))
 
-
-import datetime
-import numpy as np
-import pandas as pd
-
-from bokeh.io import show, output_notebook
-
-from bokeh.models import HoverTool, Range1d
-
-# Create dataframe of dates and random download numbers.
-startdate = datetime.datetime.now()
-nextdate = lambda x:startdate+datetime.timedelta(x)
-
-value = 10
-
-dates = [nextdate(i) for i in range(value)]
-downloads = np.random.randint(0,1000,value)
-data = np.array([dates,downloads]).T
-data = pd.DataFrame(data,columns = ["Date","Downloads"])
-data["Date"] = data.Date.apply(lambda x:"{:%Y %b %d}".format(x))
-
-# Convert dataframe to html
-data_html = data.to_html(index=False)
-
-#output_notebook()
-
-fig = figure(x_range=(0, 5), y_range=(0, 5),tools=[HoverTool(tooltips="""@html{safe}""")])
-
-source=ColumnDataSource(data=dict(x=[1,3],
-                                  y=[2,4],
-                                  html=["<b>Some other html.</b>", data_html]))
-
-fig.circle('x', 'y', size=20, source=source)
-
-show(fig)
+#show(column(p1,p2))
 
 
 # 각 지표별로 3MA, 12MA 계산하고 그에 따라 국면 부여 (R, E, S, C)
 # 지표를 부여된 국면에 따라 서로 다른 색으로 표시되도록 Plot
 
-from bokeh.transform import factor_cmap
-from bokeh.palettes import Spectral10
-from bokeh.models import ColumnDataSource
 
 macroData = pd.read_excel('test_timeseries.xlsx', sheet_name = 'macro').set_index('Date')[['OECD_CLI','ESI']]
 numIndicator = len(macroData.columns)
@@ -145,14 +110,64 @@ for i in range(numIndicator):
 
 regimes = ['Recovery','Expansion','Slowdown','Contraction']
 
-source = ColumnDataSource(data = dict(date= macroData.index.values[12:],
+source = ColumnDataSource(data = dict(date= macroData.index[12:],
                                       value= list(macroData['ESI'].values[12:]),
                                       regime = list(macroData['ESI_indic'].values[12:])))
 
-p1 = figure(plot_width=800, plot_height=300, title="simple line example",
+p3 = figure(plot_width=1600, plot_height=600, title="simple line example",
            x_axis_type="datetime", x_axis_label='Date', y_axis_label='Level')        
 
-p1.scatter("date", "value", source = source, legend = "regime",                    
-           color = factor_cmap("regime" ,palette=Spectral10, factors= regimes))        
-p1.legend.location = "top_left"
-show(p1)    
+p3.scatter("date", "value", source = source, legend = "regime",                    
+           color = factor_cmap("regime" ,palette=Spectral10, factors= regimes))      
+
+p3.add_tools(HoverTool(
+                tooltips=[('Date','@date{%F}'),('Level','@value{%0.1f}'), ('Regime', '@regime')],
+                formatters = {'date':'datetime', 'value' :'printf'},
+                mode = 'vline',
+                show_arrow=False, point_policy='follow_mouse'))  
+p3.legend.location = "top_left"
+#show(p3)  
+
+panel_1 = Panel(child = column(p1,p2), title='Panel 1')
+panel_2 = Panel(child = p3, title = 'Panel 2')
+
+tabs = Tabs(tabs = [panel_1, panel_2])
+
+show(tabs)
+
+
+#import datetime
+#import numpy as np
+#import pandas as pd
+#
+#from bokeh.io import show, output_notebook
+#
+#from bokeh.models import HoverTool, Range1d
+#
+## Create dataframe of dates and random download numbers.
+#startdate = datetime.datetime.now()
+#nextdate = lambda x:startdate+datetime.timedelta(x)
+#
+#value = 10
+#
+#dates = [nextdate(i) for i in range(value)]
+#downloads = np.random.randint(0,1000,value)
+#data = np.array([dates,downloads]).T
+#data = pd.DataFrame(data,columns = ["Date","Downloads"])
+#data["Date"] = data.Date.apply(lambda x:"{:%Y %b %d}".format(x))
+#
+## Convert dataframe to html
+#data_html = data.to_html(index=False)
+#
+##output_notebook()
+#
+#fig = figure(x_range=(0, 5), y_range=(0, 5),tools=[HoverTool(tooltips="""@html{safe}""")])
+#
+#source=ColumnDataSource(data=dict(x=[1,3],
+#                                  y=[2,4],
+#                                  html=["<b>Some other html.</b>", data_html]))
+#
+#fig.circle('x', 'y', size=20, source=source)
+#
+##show(fig)
+  
