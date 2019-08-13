3a19cb7c1000d7b89826c7b5fbf83d69882e6c65
capricorn89 <capricorn8911@gmail.com>
2019-08-13 19:06:21 +0900

    Create sendPictures.py

---
+++ XXI. ChartBot/sendPictures.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+import telegram
+from config import telegram_token_news
+
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+api_key = '969501539:AAE9y0VBLFVF-O2DGXwPNcIUMiirE17lBgg'
