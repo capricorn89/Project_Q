419865d7e811c61808adefadf7ebf075ac08dd2a
capricorn89 <capricorn8911@gmail.com>
2019-08-13 19:11:52 +0900

    Update sendPictures.py

--- XXI. ChartBot/sendPictures.py
+++ XXI. ChartBot/sendPictures.py
@@ -1,9 +1,27 @@
-from bs4 import BeautifulSoup
-import requests
-import telegram
-from config import telegram_token_news
-
 from telegram.ext import Updater, CommandHandler
 import requests
 import re
+
 api_key = '969501539:AAE9y0VBLFVF-O2DGXwPNcIUMiirE17lBgg'
+contents = requests.get('https://random.dog/woof.json').json()
+image_url = contents['url']
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+url = get_url()
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+    
+updater = Updater(api_key)
+dp = updater.dispatcher
+dp.add_handler(CommandHandler('bop',bop))
+updater.start_polling()
+updater.idle()
+    
+
